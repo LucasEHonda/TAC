413a48d4ffe065fb858671874afbf7d033911523
Lucas Honda <eliashondalucas@hotmail.com>
2021-08-04 07:03:13 -0300

    Identation

--- include/Game.h
+++ include/Game.h
@@ -5,18 +5,18 @@
 #include <string>
 
 class Game{
-private:
-    Game(std::string title, int width, int height);
-    static Game* instance;
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    // State* state;
-public:
-    ~Game();
-    void Run();
-    SDL_Renderer* GetRenderer();
-    // State& GetState();
-    static Game& GetInstance();
+    private:
+        Game(std::string title, int width, int height);
+        static Game* instance;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        // State* state;
+    public:
+        ~Game();
+        void Run();
+        SDL_Renderer* GetRenderer();
+        // State& GetState();
+        static Game& GetInstance();
 };
 
 Game::Game(std::string title, int width, int height){
